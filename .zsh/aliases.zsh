4375e4243769de1a245421a6240ad35bbc87513e
mnineteenv <90570875+m19v@users.noreply.github.com>
2023-01-05 22:21:02 +0100

    Create aliases.zsh

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,16 @@
+# Alias
+# ---
+#
+alias k="kubectl"
+alias h="helm"
+alias tf="terraform"
+alias a="ansible"
+alias ap="ansible-playbook"
+alias dt="datree"
+
+
+# ALIAS COMMANDS
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons --group-directories-first -l"
+alias g="goto"
+alias grep='grep --color'
