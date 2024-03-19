b8f37837e1db8c6fe6f0b078f9a04792f33f7a11
m19v <mnineteenv@gmail.com>
2024-03-19 09:38:31 +0100

    Add files via upload

---
+++ .config/nvim/lua/chadrc.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+M.ui = {
+  ------------------------------- base46 -------------------------------------
+  theme = "catppuccin",
+  transparency = false,
+
+  ------------------------------- nvchad_ui modules -----------------------------
+  statusline = {
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "block",
+    order = nil,
+    modules = nil,
+  },
+
+  -- lazyload it when there are 1+ buffers
+  tabufline = {
+    enabled = true,
+    lazyload = true,
+    order = { "treeOffset", "buffers", "tabs", "btns" },
+    modules = nil,
+  },
+
+}
+
+return M
---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,5 @@
+require "nvchad.options"
+
+vim.opt.colorcolumn = "80"
+vim.opt.relativenumber = true
+-- add yours here!
