f002c72024bd3a275cc9e1a6560d3abc90c9e342
m19v <mnineteenv@gmail.com>
2024-03-19 09:36:31 +0100

    Delete .config/nvim/lua/custom directory

--- .config/nvim/lua/custom/chadrc.lua
+++
@@ -1,6 +0,0 @@
--- chadrc.lua - used to override default config lua table NVChad. E.g.: To make changes to plugins, NVChad optionks
-
-local M = {}
-M.ui = {theme = 'catppuccin'}
-M.plugins = 'custom.plugins'
-return M
