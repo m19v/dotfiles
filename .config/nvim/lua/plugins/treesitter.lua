520b8a1e55a07916e15ba19cf02b0e585ddf8103
m19v <mnineteenv@gmail.com>
2024-04-15 00:27:15 +0200

    chore: delete lazyvim git repo

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+-- add more treesitter parsers
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+      "json",
+      "terraform",
+      "comment",
+      "css",
+      "java",
+    },
+  },
+}
