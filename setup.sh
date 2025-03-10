e6b4e5c289cebf625defa2b1dcda683ded414367
m19v <mnineteenv@gmail.com>
2025-03-10 22:46:20 +0100

    chore: setup.sh script added

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Array of packages
+packages=(
+  fd
+  ripgrep
+  lazygit
+  fzf
+  kubectl
+  kubectx
+  direnv
+  kubectl-cnpg
+  stow
+)
+
+# Iterate over the array and install each package
+for package in "${packages[@]}"; do
+  echo "Installing $package..."
+  /home/linuxbrew/.linuxbrew/bin/brew install "$package"
+done
+
+echo "All packages have been installed."
+
+
+# Set up completions
+mkdir -p "$HOME/.zfunc
+kubectl completion zsh > $HOME/.zfunc/_kubectl
+helm completion zsh > $HOME/.zfunc/_helm
